599c273dd260983cf9c78308d49cc4f324f1a255
otavio <otavio@sippulse.com>
2025-11-26 13:36:55 -0300

    Refactor main.py to remove unused multiprocessing code and integrate TTSWorker for improved audio processing

--- src/main.py
+++ src/main.py
@@ -1,38 +1,18 @@
-from concurrent.futures import ProcessPoolExecutor
-import torch.multiprocessing as mp
-
-from fastapi import FastAPI
+import asyncio
 import contextlib
 
-from src.core.worker import init_worker
+from fastapi import FastAPI
+
+from src.core.worker import TTSWorker
 
 from .api.router import router
-from .core.settings import settings
-
-mp.set_start_method("spawn", force=True)
-
-executor: ProcessPoolExecutor | None = None
-
-
-def get_executor() -> ProcessPoolExecutor:
-    if executor is None:
-        raise RuntimeError("Executor not initialized")
-    return executor
 
 
 @contextlib.asynccontextmanager
 async def lifespan(app: FastAPI):
-    global executor
-
-    executor = ProcessPoolExecutor(
-        max_workers=settings.NUM_WORKERS,
-        initializer=init_worker,
-        mp_context=mp.get_context("spawn"),
-    )
+    tts_worker = TTSWorker.factory()
+    asyncio.create_task(tts_worker.start_loop())
     yield
-
-    if executor:
-        executor.shutdown()
 
 
 app = FastAPI(lifespan=lifespan)

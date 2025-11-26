4cf76b5c18c45bd5351081589268b9c447fbc781
otavio <otavio@sippulse.com>
2025-11-26 13:44:01 -0300

    Refactor TTSWorker class to encapsulate model initialization and improve structure

--- src/core/worker.py
+++ src/core/worker.py
@@ -1,9 +1,39 @@
-from src.core.tts_engine import TTSInferenceEngine
+import asyncio
+from typing import Optional
+
+from src.api.schemas import AudioFormat, Speaker
+from src.core.tts_engine import SAMPLE_RATE, TTSInferenceEngine
+from src.core.utils import save_audio_to_buffer
 
 
-model = None
+class TTSWorker:
+    _instance: Optional["TTSWorker"] = None
 
+    def __init__(self):
+        self.model = TTSInferenceEngine()
+        self.queue = asyncio.Queue()
+        self.running = False
 
-def init_worker():
-    global model
-    model = TTSInferenceEngine()
+    @classmethod
+    def factory(cls):
+        if cls._instance is None:
+            cls._instance = TTSWorker()
+        return cls._instance
+
+    async def infer(self, text: str, speaker: Speaker, audio_format: AudioFormat):
+        fut = asyncio.get_event_loop().create_future()
+        await self.queue.put((text, speaker, audio_format, fut))
+        return await fut
+
+    async def start_loop(self):
+        self.running = True
+        while self.running:
+            text, speaker, audio_format, fut = await self.queue.get()
+            audio = await asyncio.to_thread(
+                self._run_infer, text, speaker, audio_format
+            )
+            fut.set_result(audio)
+
+    def _run_infer(self, text: str, speaker: Speaker, audio_format: AudioFormat):
+        audio_bytes = self.model.inference(text, speaker)
+        return save_audio_to_buffer(audio_bytes, audio_format, SAMPLE_RATE)
